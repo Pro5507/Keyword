c067b98197d86fa23e8af6ce60f59ca958b97328
Pro5507 <nhupendrakumar9162@gmail.com>
2024-10-01 19:22:21 +0530

    Keyword

---
+++ b.py
@@ -0,0 +1,7 @@
+a= str(input("Please enter something "))
+for i in a:
+    if i=='e':
+        print("Caracter is found")
+        break
+    else:
+        print("Caracter is not found")
---
+++ p.py
@@ -0,0 +1,9 @@
+for x in range(10):
+    if x%20==0:
+        print("hello")
+    elif x%3==0:
+        pass
+    elif x%5==0:
+        print("Buzz")
+    else:
+        print(x)
